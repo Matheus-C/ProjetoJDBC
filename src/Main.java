44e54d0a647e18d5d8bbe1511160dfea60b47363
Matheus Custódio <matheuscustodio2802@gmail.com>
2023-10-29 12:51:57 -0300

    Classe DaoFactory e SellerDaoJDBC criadas

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,14 @@
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 import java.time.LocalDate;
-import java.util.Date;
-import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
+        SellerDao sellerdao = DaoFactory.createSellerDao();
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(1, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, obj);
         System.out.println(seller);
